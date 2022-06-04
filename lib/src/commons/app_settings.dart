c49dfdc91997eb0393560125dd2e3dc9d564acca
Reza Fahlevi <rezarffahlevi@gmail.com>
2022-06-05 00:03:04 +0700

    App settings

--- lib/src/commons/app_settings.dart
+++ lib/src/commons/app_settings.dart
@@ -2,4 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppSettings {
   static String name = 'Quality Family';
+  
+  // // PRODUCTION
+  static String ENV = 'Production';
+  static const String BASE_URL = 'https://reza.onepeerstech.com';
+  
+  // // DEVELOPMENT
+  // static String ENV = 'Development';
+  // static const String BASE_URL = 'http://localhost:8000/api';
 }
--- lib/src/helpers/api_helper.dart
+++ lib/src/helpers/api_helper.dart
@@ -3,9 +3,10 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_qfam/src/commons/app_settings.dart';
 
 class ApiHelper {
-  static const String BASE_URL = 'https://reza.onepeerstech.com';
+  static const String BASE_URL = AppSettings.BASE_URL;
 
   BaseOptions options = BaseOptions(
     baseUrl: BASE_URL,
